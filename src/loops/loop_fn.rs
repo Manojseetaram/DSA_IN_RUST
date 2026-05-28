ce49eb5a54493ac2ecd1f9583375dc47109a3b4e
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-05-29 01:49:28 +0530

    solve the printing problem useing for , while and infinte loops

--- src/loops/loop_fn.rs
+++ src/loops/loop_fn.rs
@@ -6,7 +6,26 @@
 //print 1 to 100
 
 pub fn print_one_to_hundred() {
-    for i in 1..100.into() {
+    //for loop
+    for i in 1..=5.into() {
         println!("{i:#?}")
     }
+
+    //While loop
+    let mut n = 5;
+    while n >= 1 {
+        println!("{n:?}");
+        n -= 1;
+    }
+
+    //infinate loop
+    let mut num = 0;
+    loop {
+        num += 1;
+        if num == 6 {
+            break;
+        }
+        println!("{num:?}");
+    }
+    //Output : 1 , 2 , 3, 4, 5
 }
