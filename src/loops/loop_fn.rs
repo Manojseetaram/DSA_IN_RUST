69bbe1d177c1bb32bab50cd1c8e5351fe0f3b114
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-05-29 15:16:28 +0530

    printing one to n numbers

--- src/loops/loop_fn.rs
+++ src/loops/loop_fn.rs
@@ -22,10 +22,32 @@ pub fn print_one_to_hundred() {
     let mut num = 0;
     loop {
         num += 1;
-        if num == 6 {
+        println!("{num:?}");
+
+        if num == 5 {
             break;
         }
-        println!("{num:?}");
     }
     //Output : 1 , 2 , 3, 4, 5
+}
+pub fn print_one_to_n_numbers(n: u32) {
+    //Printing N numbers useing for loop
+    for i in 1..=n {
+        println!("{i:?}");
+    }
+    //While loop
+    let mut num = n;
+    while num >= 1 {
+        println!("{num:?}");
+        num -= 1
+    }
+
+    let mut count = 0;
+    loop {
+        count += 1;
+        println!("{count:?}");
+        if count == n {
+            break;
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod loops;
 fn main() {
     loops::loop_fn::print_one_to_hundred();
+    loops::loop_fn::print_one_to_n_numbers(10);
 }
