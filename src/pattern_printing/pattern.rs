b381354b38b9a5dfc792fd991b6bdc0e3faea9ea
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-06-02 16:02:14 +0530

    ..

--- src/pattern_printing/pattern.rs
+++ src/pattern_printing/pattern.rs
@@ -19,8 +19,14 @@ pub fn one_to_n_number_of_star(n: u32) {
         println!("**");
     }
 }
+pub fn one_to_n_number_of_four_star(n: u32) {
+    for _ in 1..=n {
+        println!("{}", "*".repeat(4));
+    }
+}
 pub fn pattern_printing() {
     one_to_n_number(5);
     one_to_n_number_star(6);
     one_to_n_number_of_star(5);
+    one_to_n_number_of_four_star(5);
 }
