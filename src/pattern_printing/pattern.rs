58bc9fa88ed5171594572395304bc97c750781d1
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-06-03 19:08:05 +0530

    solve the reverse pyramid problem

--- src/pattern_printing/pattern.rs
+++ src/pattern_printing/pattern.rs
@@ -44,12 +44,19 @@ pub fn pyramid_print(n: u32) {
         println!("{}", "*".repeat(i as usize))
     }
 }
+//Reverse Pyramid
+pub fn reverse_pyramid(n: u32) {
+    for i in (1..=n).rev() {
+        println!("{}", "*".repeat(i as usize))
+    }
+}
 pub fn pattern_printing() {
     one_to_n_number(5);
     one_to_n_number_star(6);
     one_to_n_number_of_star(5);
     one_to_n_number_of_four_star(5);
-    m_star_print_evry_row(1000, 5);
+    m_star_print_evry_row(1, 5);
     sqare_star_print(5);
     pyramid_print(8);
+    reverse_pyramid(12);
 }
