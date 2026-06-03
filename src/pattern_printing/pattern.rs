2233d1fdec3f8677801884f6659081c7880b3e33
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-06-03 17:49:12 +0530

    solve the m number of start problem

--- src/pattern_printing/pattern.rs
+++ src/pattern_printing/pattern.rs
@@ -24,9 +24,16 @@ pub fn one_to_n_number_of_four_star(n: u32) {
         println!("{}", "*".repeat(4));
     }
 }
+//Print the M stars in evry row
+pub fn m_star_print_evry_row(m: u32, n: u32) {
+    for _ in 1..=n {
+        println!("{}", "*".repeat(m.try_into().unwrap()));
+    }
+}
 pub fn pattern_printing() {
     one_to_n_number(5);
     one_to_n_number_star(6);
     one_to_n_number_of_star(5);
     one_to_n_number_of_four_star(5);
+    m_star_print_evry_row(1000, 5);
 }
