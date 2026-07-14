f6d8367d90d6aeacb7924da9080ba2c9d46a93cd
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-14 12:28:12 +0530

    input and outpur in array

--- src/arrays/arr.rs
+++ src/arrays/arr.rs
@@ -1,3 +1,33 @@
-pub fn arrays() {
-    println!("..")
+use std::io;
+
+pub fn input_output() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let n: usize = input.trim().parse().unwrap();
+
+    input.clear();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let nums: Vec<i32> = input
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+
+    let mut a = vec![0; n];
+
+    let mut i = 0;
+    while i < n {
+        a[i] = nums[i];
+        i += 1;
+    }
+
+    let mut i = 0;
+    while i < n {
+        print!("{} ", a[i]);
+        i += 1;
+    }
 }
+pub fn arrays() {
+    input_output();
+}
+
