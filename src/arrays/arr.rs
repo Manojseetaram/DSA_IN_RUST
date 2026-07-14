045475ba7e94f08c17449c38680c2adadf790e9b
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-14 15:17:55 +0530

    solve the reverse number

--- src/arrays/arr.rs
+++ src/arrays/arr.rs
@@ -1,3 +1,4 @@
+#![allow(dead_code)]
 use std::io;
 
 pub fn input_output() {
@@ -27,7 +28,24 @@ pub fn input_output() {
         i += 1;
     }
 }
-pub fn arrays() {
-    input_output();
-}
+pub fn reverse_array() {
+    let a = vec![11, 24, 87, 9];
+    let mut b = Vec::new();
 
+    let mut i = a.len();
+    while i > 0 {
+        b.push(a[i - 1]);
+        i -= 1;
+    }
+
+    let mut j = 0;
+    while j < b.len() {
+        print!("{} ", b[j]);
+        j += 1;
+    }
+    println!()
+}
+pub fn arrays() {
+    /*   input_output(); */
+    reverse_array();
+}
