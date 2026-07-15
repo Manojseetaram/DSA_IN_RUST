73f6d297df94e2bb5cb5cbc24194a047c74f4392
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-15 22:03:34 +0530

    solve 2d arrr problem

--- src/two_d_array/two_d.rs
+++ src/two_d_array/two_d.rs
@@ -1,3 +1,60 @@
+#![allow(dead_code)]
+pub fn row_to_col() {
+    let a = [[1, 2, 3, 1], [4, 5, 6, 2], [7, 8, 9, 3]];
+    let r = 3;
+    let c = 4;
+    let mut i = 0;
+    while i < r {
+        let mut j = 0;
+        while j < c {
+            print!("{}  ", a[i][j]);
+            j += 1;
+        }
+        println!();
+        i += 1;
+    }
+}
+pub fn col_to_row() {
+    let a = [[1, 2, 3, 1], [4, 5, 6, 2], [7, 8, 9, 3]];
+    let r = 3;
+    let c = 4;
+    let mut i = 0;
+    while i < c {
+        let mut j = 0;
+        while j < r {
+            print!("{}  ", a[j][i]);
+            j += 1;
+        }
+        println!();
+        i += 1;
+    }
+}
+pub fn wave_print_row_wise() {
+    let a = [[1, 2, 3, 1], [4, 5, 6, 2], [7, 8, 9, 3]];
+    let r = 3;
+    let c = 4;
+    let mut i = 0;
+    while i < r {
+        if i % 2 == 0 {
+            let mut j = 0;
+
+            while j < c {
+                print!("{}  ", a[i][j]);
+                j += 1;
+            }
+        } else {
+            let mut j = (c - 1) as i32;
+            while j >= 0 {
+                print!("{}  ", a[i][j as usize]);
+                j -= 1;
+            }
+        }
+        println!();
+        i += 1;
+    }
+}
 pub fn two_d_array() {
-    println!("This is manoj")
+    row_to_col();
+    col_to_row();
+    wave_print_row_wise();
 }
