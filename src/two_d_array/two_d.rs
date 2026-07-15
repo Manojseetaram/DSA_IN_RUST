73216e1caedba8015f5e8418bd43691e307f1837
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-15 22:38:38 +0530

    print wave col wise

--- src/two_d_array/two_d.rs
+++ src/two_d_array/two_d.rs
@@ -43,9 +43,40 @@ pub fn wave_print_row_wise() {
                 j += 1;
             }
         } else {
-            let mut j = (c - 1) as i32;
+            // let mut j = (c - 1) as i32;
+            // while j >= 0 {
+            //     print!("{}  ", a[i][j as usize]);
+            //     j -= 1;
+            // }
+            let mut j = c - 1;
+            loop {
+                print!("{}  ", a[i][j]);
+                if j == 0 {
+                    break;
+                }
+                j -= 1;
+            }
+        }
+        println!();
+        i += 1;
+    }
+}
+pub fn print_col_wise() {
+    let a = [[1, 2, 3, 1], [4, 5, 6, 2], [7, 8, 9, 3]];
+    let r = 3;
+    let c = 4;
+    let mut i = 0;
+    while i < c {
+        if i % 2 == 0 {
+            let mut j = 0;
+            while j < r {
+                print!("{}  ", a[j][i]);
+                j += 1;
+            }
+        } else {
+            let mut j = (r - 1) as i32;
             while j >= 0 {
-                print!("{}  ", a[i][j as usize]);
+                print!("{}  ", a[j as usize][i]);
                 j -= 1;
             }
         }
@@ -57,4 +88,5 @@ pub fn two_d_array() {
     row_to_col();
     col_to_row();
     wave_print_row_wise();
+    print_col_wise();
 }
