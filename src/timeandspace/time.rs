d9318cef49be0032a8a0878ea722fdffa8fbe4e8
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-22 11:32:05 +0530

    ..

--- src/timeandspace/time.rs
+++ src/timeandspace/time.rs
@@ -3,12 +3,12 @@ use std::io;
 pub fn useing_time_com_fact() {
     let mut a = String::new();
     io::stdin().read_line(&mut a).unwrap();
-    let n = a.trim().parse().unwrap();
+    let n: usize = a.trim().parse().unwrap();
+
     let mut count = 0;
     let mut i = 1;
     while i * i <= n {
         if n % i == 0 {
-            print!("{} ", i);
             let j = n / i;
             if i == j {
                 count += 1;
@@ -19,8 +19,8 @@ pub fn useing_time_com_fact() {
 
         i += 1;
     }
-    println!();
-    println!("Factors : {}", count)
+
+    println!("{}", count)
 }
 pub fn without_usieng_time_complexity() {
     let mut a = String::new();
@@ -39,7 +39,40 @@ pub fn without_usieng_time_complexity() {
     println!();
     println!("Factors : {}", count)
 }
+pub fn natural_sum() {
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).unwrap();
+    let n: usize = a.trim().parse().unwrap();
+    let mut sum = 0;
+    let mut i = 1;
+    while i <= n {
+        sum += i;
+        i += 1;
+    }
+    println!("{}", sum)
+}
+pub fn interval_sum() {
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).unwrap();
+    let t = a.trim().parse().unwrap();
+    for _ in 0..t {
+        a.clear();
+        io::stdin().read_line(&mut a).unwrap();
+        let mut n = a.trim().split_whitespace();
+
+        let z: usize = n.next().unwrap().trim().parse().unwrap();
+        let m: usize = n.next().unwrap().trim().parse().unwrap();
+
+        let count = (m - z) + 1;
+
+        let sum = (m + z) * count / 2;
+        println!("{}", sum)
+    }
+}
+pub fn counting_intervals() {}
 pub fn timeandspace() {
-    useing_time_com_fact();
-    without_usieng_time_complexity();
+    /*    useing_time_com_fact(); */
+    /*  without_usieng_time_complexity(); */
+    /*     natural_sum(); */
+    interval_sum();
 }
