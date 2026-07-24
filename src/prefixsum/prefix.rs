b80e16bf68e15473218b3c436ec3233f9367063b
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-07-24 14:59:23 +0530

    solve the vowels

--- src/prefixsum/prefix.rs
+++ src/prefixsum/prefix.rs
@@ -1,45 +1,63 @@
 #![allow(dead_code)]
 use std::io;
+
 pub fn range_sum_query() {
-    let mut a = String::new();
-    io::stdin().read_line(&mut a).unwrap();
-    let _n: usize = a.trim().parse().unwrap();
-    a.clear();
-    io::stdin().read_line(&mut a).unwrap();
-    let arr: Vec<i32> = a
-        .trim()
-        .split_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
-    a.clear();
-    io::stdin().read_line(&mut a).unwrap();
-    let t: usize = a.trim().parse().unwrap();
-    for _ in 0..t {
-        a.clear();
-        io::stdin().read_line(&mut a).unwrap();
-        let mut b = a.trim().split_whitespace();
-        let l: i32 = b.next().unwrap().trim().parse().unwrap();
-        let r: i32 = b.next().unwrap().trim().parse().unwrap();
-        let mut i = l - 1;
-        let mut sum = 0;
-        while i <= r - 1 {
-            sum += arr[i as usize];
-            i += 1;
-        }
-        println!("{}", sum)
+    let mut s = String::new();
+
+    io::stdin().read_line(&mut s).unwrap();
+    let n: usize = s.trim().parse().unwrap();
+
+    s.clear();
+    io::stdin().read_line(&mut s).unwrap();
+    let arr: Vec<i64> = s.split_whitespace().map(|x| x.parse().unwrap()).collect();
+
+    let mut p = vec![0; n];
+    p[0] = arr[0];
+    for i in 1..n {
+        p[i] = p[i - 1] + arr[i];
+    }
+
+    s.clear();
+    io::stdin().read_line(&mut s).unwrap();
+    let q: usize = s.trim().parse().unwrap();
+
+    for _ in 0..q {
+        s.clear();
+        io::stdin().read_line(&mut s).unwrap();
+        let mut it = s.split_whitespace();
+
+        let l: usize = it.next().unwrap().parse().unwrap();
+        let r: usize = it.next().unwrap().parse().unwrap();
+
+        let ans = if l == 1 {
+            p[r - 1]
+        } else {
+            p[r - 1] - p[l - 2]
+        };
+
+        println!("{ans}");
     }
 }
 pub fn even_sum_query() {
     let mut a = String::new();
     io::stdin().read_line(&mut a).unwrap();
-    let _n: usize = a.trim().parse().unwrap();
+    let n: usize = a.trim().parse().unwrap();
     a.clear();
     io::stdin().read_line(&mut a).unwrap();
-    let arr: Vec<i32> = a
+    let arr: Vec<i64> = a
         .trim()
         .split_whitespace()
         .map(|x| x.parse().unwrap())
         .collect();
+    let mut p = vec![0; n];
+    p[0] = 0;
+    for i in 1..n {
+        if (i + 1) % 2 == 0 {
+            p[i] = p[i - 1] + arr[i]
+        } else {
+            p[i] = p[i - 1];
+        }
+    }
     a.clear();
     io::stdin().read_line(&mut a).unwrap();
     let t: usize = a.trim().parse().unwrap();
@@ -47,17 +65,15 @@ pub fn even_sum_query() {
         a.clear();
         io::stdin().read_line(&mut a).unwrap();
         let mut k = a.trim().split_whitespace();
-        let l: i32 = k.next().unwrap().trim().parse().unwrap();
-        let r: i32 = k.next().unwrap().trim().parse().unwrap();
-        let mut i = l - 1;
-        let mut sum = 0;
-        while i <= r - 1 {
-            if (i + 1) % 2 == 0 {
-                sum += arr[i as usize] as i64;
-            }
-            i += 1;
-        }
-        println!("{}", sum)
+        let l: usize = k.next().unwrap().trim().parse().unwrap();
+        let r: usize = k.next().unwrap().trim().parse().unwrap();
+
+        let range_sum = if l == 1 {
+            p[r - 1]
+        } else {
+            p[r - 1] - p[l - 2]
+        };
+        println!("{range_sum}")
     }
 }
 pub fn count_vowels() {
@@ -67,6 +83,19 @@ pub fn count_vowels() {
     a.clear();
     io::stdin().read_line(&mut a).unwrap();
     let str_line: Vec<char> = a.trim().chars().collect();
+    let mut p = vec![0; n];
+
+    if matches!(str_line[0], 'a' | 'e' | 'i' | 'o' | 'u') {
+        p[0] = 1;
+    }
+
+    for i in 1..n {
+        if matches!(str_line[i], 'a' | 'e' | 'i' | 'o' | 'u') {
+            p[i] = p[i - 1] + 1;
+        } else {
+            p[i] = p[i - 1];
+        }
+    }
     a.clear();
     io::stdin().read_line(&mut a).unwrap();
     let t: usize = a.trim().parse().unwrap();
@@ -74,12 +103,36 @@ pub fn count_vowels() {
         a.clear();
         io::stdin().read_line(&mut a).unwrap();
         let mut b = a.trim().split_whitespace();
-        let l: i64 = b.next().unwrap().trim().parse().unwrap();
-        let r: i64 = b.next().unwrap().trim().parse().unwrap();
+        let l: usize = b.next().unwrap().trim().parse().unwrap();
+        let r: usize = b.next().unwrap().trim().parse().unwrap();
+        let count = if l == 1 {
+            p[r - 1]
+        } else {
+            p[r - 1] - p[l - 2]
+        };
+        println!("{}", count)
     }
 }
 pub fn prefix_sum() {
-    /* range_sum_query(); */
-    /*   even_sum_query(); */
+    /*    range_sum_query(); */
+    /*    even_sum_query(); */
     count_vowels();
+
+    //
+    //  let mut p = vec![0; n];
+    // p[0] = str_line[0];
+    // for i in 1..n {
+    //     if !matches!(str_line[i], 'a' | 'e' | 'i' | 'o' | 'u') {
+    //         p[i] = p[i - 1] + str_line[i]
+    //     } else {
+    //         p[i] = p[i - 1]
+    //     }
+    // }
+    //  let ans = if l == 1 {
+    //     p[r - 1]
+    // } else {
+    //     p[r - 1] - p[l - 2]
+    // };
+    // println!("ans")
+    //
 }
