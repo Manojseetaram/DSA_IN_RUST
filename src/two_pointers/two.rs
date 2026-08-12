a98b20a993d0ca842e24b4fdd4da79973efa5da7
Manojseetaram <manojseetaram.artytech@gmail.com>
2026-08-12 11:34:10 +0530

    sum off evry sub array

--- src/two_pointers/two.rs
+++ src/two_pointers/two.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
 use std::io;
-pub fn two_pointers() {
+pub fn carry_forword_method() {
     //Useing carry Forward Technique
     //
     let mut a = String::new();
@@ -29,4 +29,38 @@ pub fn two_pointers() {
         println!();
         l += 1;
     }
+}
+pub fn sum() {
+    let mut a = String::new();
+    io::stdin().read_line(&mut a).unwrap();
+    let n: usize = a.trim().parse().unwrap();
+    a.clear();
+    io::stdin().read_line(&mut a).unwrap();
+    let arr: Vec<i64> = a
+        .trim()
+        .split_whitespace()
+        .map(|x| x.parse().unwrap())
+        .collect();
+    let mut l = 0;
+    while l < n {
+        let mut r = l;
+        while r < n {
+            let mut sum = 0;
+            let mut i = l;
+
+            while i <= r {
+                sum += arr[i];
+
+                i += 1;
+            }
+            print!("{} ", sum);
+            r += 1;
+        }
+        println!();
+        l += 1;
+    }
+}
+pub fn two_pointers() {
+    /*     carry_forword_method(); */
+    sum();
 }
